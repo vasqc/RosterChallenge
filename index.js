32ada07edf0b506644aa1a0a16c0ab7e645670c0
vasqc <90007988+vasqc@users.noreply.github.com>
2022-01-09 21:15:08 -0500

    basic info

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "Roster.html");
 
-const render = require("./page-template.js");
+const render = require("./page-temp");
 
 const teamMembers = [];
 const idArray = [];
